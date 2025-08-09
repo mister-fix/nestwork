b02d6ef79a83ce48f57393b944e2272a017310f7
Stephen <hellostephenwm@gmail.com>
2025-08-09 19:22:19 -0400

    feat: scaffold Nestwork's web app with Next.js

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import '@repo/ui/styles/globals.css';
+
+const inter = Inter({
+	subsets: ['latin'],
+	variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+	description:
+		'Nestwork is a modern task management platform built for high-performance teams.',
+	icons: {
+		apple: '/apple-icon.png',
+	},
+	title: {
+		default: 'Nestwork',
+		template: '%s | Nestwork',
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
