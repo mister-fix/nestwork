3d9e95c2caefd88ac47a59f892da33124c3f575a
Stephen <hellostephenwm@gmail.com>
2025-08-12 14:01:29 -0400

    chore: update verification email template

--- apps/web/components/emails/verification-email.tsx
+++ apps/web/components/emails/verification-email.tsx
@@ -1,4 +1,16 @@
-import { Button, Head, Html } from '@react-email/components';
+import {
+	Body,
+	Button,
+	Container,
+	Head,
+	Hr,
+	Html,
+	Img,
+	Link,
+	Preview,
+	Section,
+	Text,
+} from '@react-email/components';
 import { Tailwind } from '@react-email/tailwind';
 
 interface EmailProps {
@@ -7,13 +19,69 @@ interface EmailProps {
 }
 
 export function VerificationEmail(props: EmailProps) {
-	const { verificationUrl } = props;
+	const { name, verificationUrl } = props;
 
 	return (
 		<Html lang="en">
 			<Tailwind>
 				<Head />
-				<Button href={verificationUrl}>Click me</Button>
+				<Body>
+					<Preview>The workspace platform that supercharges your team.</Preview>
+					<Container>
+						<Section className="border-b border-neutral-300">
+							<Img
+								className="mx-auto my-0"
+								alt="Nestwork logo"
+								src={`${process.env.NEXT_PUBLIC_BASE_URL}/nestwork-primary.png`}
+								height={28}
+								width={120}
+							/>
+							<Hr className="mx-0 my-5 border-neutral-300" />
+
+							<Text>Hi {name?.split(' ')[0]},</Text>
+							<Text>
+								Welcome to Nestwork, the modern task management platform built
+								for high-performance teams. We&apos;re excited to help you
+								supercharge your workflow.
+							</Text>
+							<Text>
+								Click the button below to verify your account and create a
+								workspace.
+							</Text>
+							<Button
+								className="block rounded-md bg-[#0e0c7c] px-6 py-3 text-sm font-semibold text-white transition-all duration-150 hover:bg-[#0e0c7c]/80"
+								href={verificationUrl}
+							>
+								Get started
+							</Button>
+							<Text>
+								When you get a chance, consider viewing our{' '}
+								<Link href="https://nestwork-docs.vercel.app/">docs</Link> for
+								more info on how you can maximize your experience.
+							</Text>
+							<Text>
+								We are always available to answer any questions and provide
+								support, you can{' '}
+								<Link href="https://nestwork-web.vercel.app/">contact us</Link>{' '}
+								at any time. We also have a{' '}
+								<Link href="https://nestwork-web.vercel.app/">
+									support page
+								</Link>{' '}
+								where you may be able to find answers to some of your questions.
+							</Text>
+							<Text>
+								Thank you for becoming a member, and we look forward to seeing
+								your growth in productivity in the future.
+							</Text>
+							<Text>&mdash; The Nestwork Team.</Text>
+						</Section>
+						<Section className="text-center">
+							<Text className="text-xs text-gray-400">
+								&copy; {new Date().getFullYear()} Nestwork. All rights reserved.
+							</Text>
+						</Section>
+					</Container>
+				</Body>
 			</Tailwind>
 		</Html>
 	);
