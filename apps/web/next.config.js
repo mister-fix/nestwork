6c53a91924c388ebabf8334e82405602fb983944
Stephen <hellostephenwm@gmail.com>
2025-08-12 01:09:42 -0400

    feat: add 'better-auth', and '@hookform/resolvers' to transpile array

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ['@repo/ui', '@repo/assets'],
+	transpilePackages: [
+		'@repo/ui',
+		'@repo/assets',
+		'better-auth',
+		'@hookform/resolvers',
+	],
 };
 
 export default nextConfig;
